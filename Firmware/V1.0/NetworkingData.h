669ddb47f646441ff56338964e4f982bb7358cb4
eGuardianDev <cvetomir200419@abv.bg>
2022-11-06 16:54:21 +0200

    Adding this

---
+++ Firmware/V1.0/NetworkingData.h
@@ -0,0 +1,14 @@
+
+//Here you can input you data for the network
+// this is done for better git control and not to steal my wifi password lol
+
+
+
+class NetworkData{
+  public:
+   static String GetNetworkName() { return ""; }
+   static String GetNetworkPass() { return ""; }
+  private:
+
+  
+};
